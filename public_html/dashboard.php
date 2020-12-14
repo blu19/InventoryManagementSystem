0156f5190a4d5321f95406ed2c5fdff7d28eeb22
BrianU <brian.ulatowski@gmail.com>
2020-12-14 17:26:58 +0700

    added cards for profile, orders, categories, brands, products

--- public_html/dashboard.php
+++ public_html/dashboard.php
@@ -21,7 +21,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <div class="card mx-auto" style="width: 20rem;">
+                <div class="card mx-auto">
                   <img src="./images/user.png" style="width:60%" class="card-img-top mx-auto" alt="Card image cap">
                   <div class="card-body">
                     <h4 class="card-title"></i>Profile Info</h4>
@@ -41,14 +41,55 @@
                                 <iframe src="http://free.timeanddate.com/clock/i7l8nwj9/n199/szw160/szh160/hoca32/hbw10/hfcc00/cf100/hnca32/fas30/facfff/fdi86/mqcfff/mqs2/mql3/mqw4/mqd70/mhcfff/mhs2/mhl3/mhw4/mhd70/mmcfff/mml4/mmw4/mmd98/hhcfff/hhs2/hmcfff/hms2/hscfff" frameborder="0" width="160" height="160"></iframe>
                             </div>
                             <div class="col-sm-6">
-                            
+                                <div class="card">
+                                    <div class="card-body">
+                                      <h4 class="card-title">New Orders</h4>
+                                      <p class="card-text">Here you can make new invoices and create new orders.</p>
+                                      <a href="#" class="btn btn-primary">New Orders</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                 </div>
             </div>
         </div>
     </div>
-
+    <p></p>
+    <p></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                      <h4 class="card-title">Categories</h4>
+                      <p class="card-text">Here you can manage your categories and can add parent and sub categories</p>
+                      <a href="#" class="btn btn-primary">Add</a>
+                      <a href="#" class="btn btn-primary">Manage</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                      <h4 class="card-title">Brands</h4>
+                      <p class="card-text">Here you can manage your brand and can add a new brand</p>
+                      <a href="#" class="btn btn-primary">Add</a>
+                      <a href="#" class="btn btn-primary">Manage</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                      <h4 class="card-title">Products</h4>
+                      <p class="card-text">Here you can manage your products and can add new products</p>
+                      <a href="#" class="btn btn-primary">Add</a>
+                      <a href="#" class="btn btn-primary">Manage</a>
+                    </div>
+                </div>
+            </div>
+        </div>                            
+    </div>
 
 
 </body>
